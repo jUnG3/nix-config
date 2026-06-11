d26a00fd55ea0071419d1a85781ddb47f79cdec1
jUnG3 <jUnG3@users.noreply.github.com>
2026-06-11 08:32:40 +0200

    Groups configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   nix.settings.experimental-features = [
@@ -11,110 +11,152 @@
     networkmanager.enable = true;
   };
 
-  programs.ssh.startAgent = true;
-
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
+  programs = {
+    ssh.startAgent = true;
+    zsh.enable = true;
+    users.defaultUserShell = pkgs.zsh;
+    gamemode = {
+      enable = true;
+    };
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    dconf = {
+      enable = true;
+    };
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+  };
 
   time.timeZone = "Europe/Zagreb";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ALL = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ALL = "en_US.UTF-8";
+    };
   };
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelParams = [
+      "amd_pstate=active"
+    ];
+    initrd = {
+      kernelModules = [ "amdgpu" ];
+      services = {
+        usbmuxd = {
+          enable = true;
+        };
+        xserver = {
+          videoDrivers = [ "amdgpu" ];
+        };
+      };
+    };
   };
 
-  hardware.cpu.amd.updateMicrocode = true;
-  boot.kernelParams = [
-    "amd_pstate=active"
-  ];
+  hardware = {
+    cpu.amd.updateMicrocode = true;
+    sane.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = [
+        pkgs.rocmPackages.clr.icd
+        pkgs.rocmPackages.hipcc
+      ];
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      # Optional but nice (battery reporting, etc.)
+      settings.General = {
+        Experimental = true;
+      };
+    };
+  };
+
   powerManagement.cpuFreqGovernor = "schedutil";
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  users = {
+    users.junge = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "audio"
+        "video"
+        "input"
+        "scanner"
+        "lpadmin"
+      ];
+    };
+    mutableUsers = true;
+  };
+
   services = {
-    usbmuxd = {
+    pulseaudio.enable = false;
+    blueman.enable = true;
+    openssh.enable = false;
+    rtkit.enable = true;
+    xserver.enable = false;
+
+    pipewire = {
       enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
     };
-    xserver = {
-      videoDrivers = [ "amdgpu" ];
+
+    greetd = {
+      enable = true;
+      settings.default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+
+        user = "greeter";
+      };
+    };
+
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplipWithPlugin ];
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
     };
   };
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = [
-      pkgs.rocmPackages.clr.icd
-      pkgs.rocmPackages.hipcc
+
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    };
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
     ];
-  };
-  programs.gamemode.enable = true;
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-  programs.dconf.enable = true;
-
-  users.users.junge = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "audio"
-      "video"
-      "input"
-      "scanner"
-      "lpadmin"
-    ];
-  };
-
-  users.mutableUsers = true;
-
-  services.pulseaudio.enable = false;
-  services.blueman.enable = true;
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-
-    wireplumber.enable = true;
-  };
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    # Optional but nice (battery reporting, etc.)
-    settings.General = {
-      Experimental = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Fira Code" ];
+        sansSerif = [ "Fira Code" ];
+        monospace = [ "Fira Code Mono" ];
+      };
     };
-  };
-
-  services.xserver.enable = false;
-  services.greetd = {
-    enable = true;
-    settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-      user = "greeter";
-    };
-  };
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
 
   environment.systemPackages = with pkgs; [
@@ -135,33 +177,7 @@
     blueman
   ];
 
-  services.openssh.enable = false;
-
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.11";
 
-  fonts = {
-    packages = with pkgs; [
-      nerd-fonts.fira-code
-      nerd-fonts.droid-sans-mono
-    ];
-
-    fontconfig = {
-      defaultFonts = {
-        serif = [ "Fira Code" ];
-        sansSerif = [ "Fira Code" ];
-        monospace = [ "Fira Code Mono" ];
-      };
-    };
-  };
-
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-  };
-  hardware.sane.enable = true;
 }
