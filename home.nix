7494ef108523243eeacd766367f1ace12c981726
jUnG3 <jUnG3@users.noreply.github.com>
2026-06-18 10:21:58 +0200

    [wl-kbptr] Uses config file the right way

--- home.nix
+++ home.nix
@@ -398,7 +398,7 @@ in
       '';
 
       "emacs/init.el".source = ./emacs/init.el;
-      "wl-kbptr/config" = ./wl-kbptr/config;
+      "wl-kbptr/config".source = ./wl-kbptr/config;
     };
   };
 
