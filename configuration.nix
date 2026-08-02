195c6f19cda1f1afe6f33fc33bebde2737e1e0bd
jUnG3 <jUnG3@users.noreply.github.com>
2026-08-02 22:28:11 +0200

    Removes ollama

--- configuration.nix
+++ configuration.nix
@@ -264,21 +264,6 @@ in
       enable = true;
       nssmdns4 = true;
     };
-
-    ollama = {
-      enable = true;
-      acceleration = "rocm";
-      package = pkgs.ollama-rocm;
-      environmentVariables = {
-        HSA_OVERRIDE_GFX_VERSION = "10.1.0";
-        OLLAMA_CONTEXT_LENGTH = "2048";
-        OLLAMA_FLASH_ATTENTION = "false";
-        OLLAMA_HOST = "127.0.0.1:11434";
-        OLLAMA_MAX_LOADED_MODELS = "1";
-        OLLAMA_NUM_PARALLEL = "1";
-        OLLAMA_KEEP_ALIVE = "0";
-      };
-    };
   };
 
   xdg.portal = {
