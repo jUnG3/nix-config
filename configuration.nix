58ecb824c09a2e07e581bd94665f4e12bf6a0616
jUnG3 <jUnG3@users.noreply.github.com>
2026-09-15 10:28:13 +0200

    Fixes xdg-desktop hangling

--- configuration.nix
+++ configuration.nix
@@ -271,7 +271,17 @@ in
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
     ];
-    config.common.default = "*";
+    config = {
+      common = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
+      };
+    };
   };
 
   fonts = {
